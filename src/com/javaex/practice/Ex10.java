9e9d92b84fa1cdfcac5d8066224139c22851d9eb
ol.season <ol.season@172.30.1.57>
2021-05-27 01:28:50 +0900

    05/27_practice02

---
+++ src/com/javaex/practice/Ex10.java
@@ -0,0 +1,28 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex10 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("사번(정수)를 입력해주세요 ");
+		System.out.print("사번:  ");
+		int num1 = sc.nextInt();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
